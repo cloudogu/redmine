8dfea57fd8003d175f479646045b1726502a92c4
Maren Süwer <maren.suewer@triology.de>
2017-10-24 14:56:01 +0200

    prepare it configuration for ci

--- spec/it/config.js
+++ spec/it/config.js
@@ -1,5 +1,16 @@
+let cesUrl = process.env.CES_URL;
+if (!cesUrl) {
+  // url from ecosystem with private network
+  cesUrl = "https://192.168.42.2"
+}
+
+let webdriverType = process.env.WEBDRIVER;
+if (!webdriverType) {
+  webdriverType = 'local';
+}
+
 module.exports = {
-    baseUrl: 'https://demo.cloudogu.com',
+    baseUrl: cesUrl,
     redmineContextPath: '/redmine',
     username: 'ces-admin',
     password: 'ecosystem2016',
@@ -7,5 +18,6 @@ module.exports = {
     lastname: 'admin',
     displayName: 'admin',
     email: 'ces-admin@cloudogu.com',
+    webdriverType: webdriverType,
     debug: true
   };
--- spec/it/utils.js
+++ spec/it/utils.js
@@ -4,10 +4,22 @@ const webdriver = require('selenium-webdriver');
 const By = webdriver.By;
 
 exports.createDriver = function(){
+    if (config.webdriverType === 'local') {
+        return createLocalDriver();
+    }
+    return createRemoteDriver();
+};
+
+function createRemoteDriver() {
     return new webdriver.Builder()
+    .build();
+}
+
+function createLocalDriver() {
+  return new webdriver.Builder()
     .withCapabilities(webdriver.Capabilities.chrome())
     .build();
-};
+}
 
 exports.login = function(driver, relativeUrl) {
     driver.get(config.baseUrl + relativeUrl);
