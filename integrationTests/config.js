29416646851f9458b594f85dadf8cb3a44b6a22f
Maren Süwer <maren.suewer@triology.de>
2017-11-24 07:40:29 +0100

    renaming

--- integrationTests/config.js
+++ integrationTests/config.js
@@ -1,7 +1,7 @@
 let cesFqdn = process.env.CES_FQDN;
 if (!cesFqdn) {
   // url from ecosystem with private network
-  cesFqdn = "192.168.115.95"//"192.168.42.2"
+  cesFqdn = "192.168.42.2"
 }
 
 let webdriverType = process.env.WEBDRIVER;
@@ -13,13 +13,13 @@ module.exports = {
     fqdn: cesFqdn,
     baseUrl: 'https://' + cesFqdn,
     redmineContextPath: '/redmine',
-    username: 'admin',//'ces-admin',
-    password: 'adminpw',//'ecosystem2016',
+    username: 'ces-admin',
+    password: 'ecosystem2016',
     firstname: 'admin',
     lastname: 'admin',
     displayName: 'admin',
-    email: 'admin@test.de',//'ces-admin@cloudogu.com',
+    email: 'ces-admin@cloudogu.com',
     webdriverType: webdriverType,
     debug: true,
-    adminGroup: 'admin'//'CesAdministrators'
+    adminGroup: 'CesAdministrators'
 };
