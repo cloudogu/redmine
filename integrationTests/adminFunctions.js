57a4aaed125989abefa6e0e0bee4b7b4c697fb90
Maren Süwer <maren.suewer@triology.de>
2018-03-07 10:38:35 +0100

    change wait timeout for linkText, Testuser to 5000

--- integrationTests/adminFunctions.js
+++ integrationTests/adminFunctions.js
@@ -45,7 +45,7 @@ module.exports = class AdminFunctions{
         await utils.login(this.driver, '/redmine');
         //delete user in redmine
         await this.driver.get(config.baseUrl + '/redmine/users');
-        await this.driver.wait(until.elementLocated(By.linkText(this.testuserName)), 20000);
+        await this.driver.wait(until.elementLocated(By.linkText(this.testuserName)), 5000);
         await this.driver.findElement(By.linkText(this.testuserName)).click();
         await this.driver.wait(until.elementLocated(By.css('a.icon.icon-del')), 5000);
         await this.driver.findElement(By.css('a.icon.icon-del')).click();
@@ -73,7 +73,7 @@ module.exports = class AdminFunctions{
         await utils.login(this.driver, '/redmine');
         await this.driver.get(config.baseUrl + '/redmine/users');
 
-        await this.driver.wait(until.elementLocated(By.linkText(this.testuserName)), 20000);
+        await this.driver.wait(until.elementLocated(By.linkText(this.testuserName)), 5000);
         await this.driver.findElement(By.linkText(this.testuserName)).click();
         await this.driver.wait(until.elementLocated(By.css('input[type="checkbox"]')), 5000);
         var buttonEnabled = await this.driver.findElement(By.css('input#user_admin')).isSelected();
@@ -88,7 +88,7 @@ module.exports = class AdminFunctions{
     async takeAdminRightsInRedmine(){
         await utils.login(this.driver, '/redmine');
         await this.driver.get(config.baseUrl + '/redmine/users');
-        await this.driver.wait(until.elementLocated(By.linkText(this.testuserName)), 20000);
+        await this.driver.wait(until.elementLocated(By.linkText(this.testuserName)), 5000);
         await this.driver.findElement(By.linkText(this.testuserName)).click();
         await this.driver.wait(until.elementLocated(By.css('input#user_admin')), 5000);
         var buttonEnabled = await this.driver.findElement(By.css('input#user_admin')).isSelected();
