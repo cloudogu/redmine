7a51c33febdada8b8a3f1843aebeda5189eff532
Simon Klein <simon.klein@cloudogu.com>
2021-04-01 16:03:24 +0200

    #49 Handle errors correctly when creating tmp admin

--- resources/create-admin.sh
+++ resources/create-admin.sh
@@ -6,13 +6,26 @@ set -o pipefail
 USERNAME="${1}"
 PASSWORD="${2}"
 
-RAILS_ENV=production bundle exec rails console <<< "
-user = User.new(:language => Setting.default_language, :mail_notification => Setting.default_notification_option, :admin => true)
-user.login = '${USERNAME}'
-user.password = '${PASSWORD}'
-user.password_confirmation = '${PASSWORD}'
-user.lastname = '${USERNAME}'
-user.firstname = '${USERNAME}'
-user.mail = '${USERNAME}@${USERNAME}.de'
-user.save!
-" >> /dev/null
+OUTPUT="$(RAILS_ENV=production bundle exec rails console <<< "
+begin
+  user = User.new(:language => Setting.default_language, :mail_notification => Setting.default_notification_option, :admin => true)
+  user.login = '${USERNAME}'
+  user.password = '${PASSWORD}'
+  user.password_confirmation = '${PASSWORD}'
+  user.lastname = '${USERNAME}'
+  user.firstname = '${USERNAME}'
+  user.mail = '${USERNAME}@${USERNAME}.de'
+  user.save!
+rescue => error
+  puts error.message
+  raise 'User was not saved'
+end
+")"
+
+
+# There is no way to get an exit code on error. So we check if exception raised text appears and exit manually.
+if [[ "${OUTPUT}" == *"RuntimeError (User was not saved)"* ]]; then
+  echo "Could not create temporary admin user due to error: "
+  printf '%s\n' "${OUTPUT#*end}"
+  exit 1
+fi
