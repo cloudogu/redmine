dfadf887eb89ba96f90d8f4dad503ce589da9432
Philipp Pixel <boris.schaa@cloudogu.com>
2021-11-12 15:22:57 +0100

    #68 secure pre- and post-upgrade scripts with source guards
    When sourced (with `source` or `.`) without further measures, shell scripts are
    executed "AS-IS". Source guards prevent a general execution when scripts are
    source. There is no difference (in terms of the execution) when the shell
    script is executed via the shell.

--- resources/post-upgrade.sh
+++ resources/post-upgrade.sh
@@ -17,45 +17,52 @@ set -o pipefail
 # shellcheck disable=SC1091
 source /util.sh
 
-FROM_VERSION="${1}"
-TO_VERSION="${2}"
+function run_postupgrade() {
+  FROM_VERSION="${1}"
+  TO_VERSION="${2}"
 
-echo "Executing Redmine post-upgrade from ${FROM_VERSION} to ${TO_VERSION}"
+  echo "Executing Redmine post-upgrade from ${FROM_VERSION} to ${TO_VERSION}"
 
-if [ "${FROM_VERSION}" = "${TO_VERSION}" ]; then
-  echo "FROM and TO versions are the same; Exiting..."
-  exit 0
+  if [ "${FROM_VERSION}" = "${TO_VERSION}" ]; then
+    echo "FROM and TO versions are the same; Exiting..."
+    exit 0
+  fi
+
+  if [[ "${FROM_VERSION}" =~ ^v3.*$ || "${FROM_VERSION}" =~ ^[v]*4.((0.5-1)|(1.0-[123]))$ ]]; then
+    # This was added due to issue #42. There can be duplicated settings in the database. This sql statement will remove them.
+    DELETE_DUPLICATE_STATEMENT="DELETE FROM settings WHERE id IN (SELECT id FROM settings WHERE NOT id IN (SELECT max(id) FROM settings GROUP BY name HAVING count(*) > 1) AND name IN (SELECT name FROM settings GROUP BY name HAVING count(name) > 1))"
+    echo "post-upgrade: Deleting duplicate settings in database..."
+    sql "${DELETE_DUPLICATE_STATEMENT}"
+  fi
+
+  echo "Making sure config/secrets.yml exists..."
+  create_secrets_yml
+
+  echo "Rendering config.ru template..."
+  render_config_ru_template
+
+  echo "Rendering database.yml template..."
+  render_database_yml_template
+
+  echo "Generating configuration.yml from template..."
+  render_configuration_yml_template
+
+  echo "Migrating database..."
+  exec_rake db:migrate
+
+  echo "Migrating plugins..."
+  exec_rake redmine:plugins:migrate
+
+  echo "Clearing cache..."
+  exec_rake tmp:cache:clear
+
+  echo "Set registry flag so startup script can start afterwards..."
+  doguctl state "upgrade done"
+
+  echo "Redmine post-upgrade done"
+}
+
+# make the script only run when executed, not when sourced from bats tests)
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  run_postupgrade "$@"
 fi
-
-if [[ "${FROM_VERSION}" =~ ^v3.*$ || "${FROM_VERSION}" =~ ^[v]*4.((0.5-1)|(1.0-[123]))$ ]]; then
-  # This was added due to issue #42. There can be duplicated settings in the database. This sql statement will remove them.
-  DELETE_DUPLICATE_STATEMENT="DELETE FROM settings WHERE id IN (SELECT id FROM settings WHERE NOT id IN (SELECT max(id) FROM settings GROUP BY name HAVING count(*) > 1) AND name IN (SELECT name FROM settings GROUP BY name HAVING count(name) > 1))"
-  echo "post-upgrade: Deleting duplicate settings in database..."
-  sql "${DELETE_DUPLICATE_STATEMENT}"
-fi
-
-echo "Making sure config/secrets.yml exists..."
-create_secrets_yml
-
-echo "Rendering config.ru template..."
-render_config_ru_template
-
-echo "Rendering database.yml template..."
-render_database_yml_template
-
-echo "Generating configuration.yml from template..."
-render_configuration_yml_template
-
-echo "Migrating database..."
-exec_rake db:migrate
-
-echo "Migrating plugins..."
-exec_rake redmine:plugins:migrate
-
-echo "Clearing cache..."
-exec_rake tmp:cache:clear
-
-echo "Set registry flag so startup script can start afterwards..."
-doguctl state "upgrade done"
-
-echo "Redmine post-upgrade done"
--- resources/pre-upgrade.sh
+++ resources/pre-upgrade.sh
@@ -3,19 +3,26 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-FROM_VERSION="${1}"
-TO_VERSION="${2}"
+function run_preupgrade() {
+  FROM_VERSION="${1}"
+  TO_VERSION="${2}"
 
-echo "Executing Redmine pre-upgrade from ${FROM_VERSION} to ${TO_VERSION}"
+  echo "Executing Redmine pre-upgrade from ${FROM_VERSION} to ${TO_VERSION}"
 
-if [ "${FROM_VERSION}" = "${TO_VERSION}" ]; then
-  echo "FROM and TO versions are the same; Exiting..."
-  exit 0
+  if [ "${FROM_VERSION}" = "${TO_VERSION}" ]; then
+    echo "FROM and TO versions are the same; Exiting..."
+    exit 0
+  fi
+
+  echo "Set registry flag so startup script waits for post-upgrade to finish..."
+  doguctl state "upgrading"
+
+  doguctl config "startup/setup_done" "true"
+
+  echo "Redmine pre-upgrade done"
+}
+
+# make the script only run when executed, not when sourced from bats tests)
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+  run_preupgrade "$@"
 fi
-
-echo "Set registry flag so startup script waits for post-upgrade to finish..."
-doguctl state "upgrading"
-
-doguctl config "startup/setup_done" "true"
-
-echo "Redmine pre-upgrade done"
