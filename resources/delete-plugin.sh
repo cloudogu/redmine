100df7f28b7ebc9977391136b1b51b782343580c
Vincent <vincent.poneleit@cloudogu.com>
2022-01-11 15:42:57 +0100

    #80 automated delete plugin
    - plugins can now be deleted via flag in cesapp cmd

--- resources/delete-plugin.sh
+++ resources/delete-plugin.sh
@@ -3,6 +3,11 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+if [[ $# -ne 1 ]]; then
+    echo "Illegal number of arguments - plugin_name must be given as first argument"
+    exit 1
+fi
+
 plugin_name=$1
 echo "delete plugin ${plugin_name}"
 bundle exec rake redmine:plugins:migrate NAME=${plugin_name} VERSION=0 RAILS_ENV=production
