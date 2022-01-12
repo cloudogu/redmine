c3850dfc572d48c10e2d3f200879f1d2b98fa884
Florian Gill <florian.gill@cloudogu.com>
2022-01-12 09:36:49 +0100

    Add comment with link to redmine wiki #80

--- resources/delete-plugin.sh
+++ resources/delete-plugin.sh
@@ -10,6 +10,8 @@ fi
 
 plugin_name=$1
 
+# Automate uninstall steps from official Redmine guide -
+# https://www.redmine.org/projects/redmine/wiki/plugins#Uninstalling-a-plugin
 echo "Delete plugin ${plugin_name}"
 bundle exec rake redmine:plugins:migrate NAME=${plugin_name} VERSION=0 RAILS_ENV=production
 rm -rf /usr/share/webapps/redmine/plugins/${plugin_name}
