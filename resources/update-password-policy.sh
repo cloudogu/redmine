9b013cbc9c146e91338ad720dece436df3fa4cd5
Georg <georg.goering-extern@cloudogu.com>
2025-01-07 15:26:28 +0100

    #141 add bats-tests and refactor
    refactoring code and adding bats tests

--- resources/update-password-policy.sh
+++ resources/update-password-policy.sh
@@ -4,19 +4,19 @@ set -o nounset
 set -o pipefail
 
 function update_password_policy_setting(){
-  echo "updating password policy in redmine start"
+  echo "Updating password policy in redmine start"
 
-  echo "retrieving config values start"
+  echo "Retrieving config values start"
   local MIN_LENGTH=$(get_password_policy_key "min_length")
   local UPPERCASE_FLAG=$(get_password_policy_key "must_contain_capital_letter")
   local LOWERCASE_FLAG=$(get_password_policy_key "must_contain_lower_case_letter")
   local DIGITS_FLAG=$(get_password_policy_key "must_contain_digit")
   local SPECIAL_FLAG=$(get_password_policy_key "must_contain_special_character")
-  echo "retrieving config values end"
+  echo "Retrieving config values end"
 
   if [[ -z "$MIN_LENGTH" && -z "$UPPERCASE_FLAG" && -z "$LOWERCASE_FLAG" && -z "$DIGITS_FLAG" && -z "$SPECIAL_FLAG" ]]; then
     echo "INFO: no password policy configuration found."
-    local SETTINGS_JSON=$(build_json "" "" "" "" "")
+    local SETTINGS_JSON=$(build_json "8" "" "" "" "")
     # clean up json in case any of the array vals are not filled
     safe_extended_api_call "settings" "PUT" "${SETTINGS_JSON}" "204"
     return 0;
@@ -47,12 +47,12 @@ function update_password_policy_setting(){
   fi
 
   local SETTINGS_JSON=$(build_json "${MIN_LENGTH}" "${UPPERCASE}" "${LOWERCASE}" "${DIGITS}" "${SPECIAL}")
-  echo "calling extended rest api start"
+  echo "Calling extended rest api start"
 
   # clean up json in case any of the array vals are not filled
   safe_extended_api_call "settings" "PUT" "${SETTINGS_JSON}" "204"
-  echo "calling extended rest api end"
-  echo "updating password policy in redmine end"
+  echo "Calling extended rest api end"
+  echo "Updating password policy in redmine end"
 }
 
 function get_password_policy_key(){
@@ -62,12 +62,7 @@ function get_password_policy_key(){
 }
 
 function build_json(){
-    local SETTINGS_JSON='
-    {
-      "password_min_length": "%s",
-      "password_required_char_classes": ["%s", "%s", "%s", "%s"]
-    }
-    '
+    local SETTINGS_JSON='{"password_min_length":"%s","password_required_char_classes":["%s","%s","%s","%s"]}'
 
     local MIN_LENGTH="${1}"
     local UPPERCASE="${2}"
