8d4b4e36829d922788a054a72000815f2921776e
Georg <georg.goering-extern@cloudogu.com>
2025-01-09 09:11:41 +0100

    #141 shellcheck fixes

--- resources/update-password-policy.sh
+++ resources/update-password-policy.sh
@@ -5,17 +5,18 @@ set -o pipefail
 
 function update_password_policy_setting(){
   echo "Updating password policy in Redmine start"
-
   echo "Retrieving config values start"
-  local MIN_LENGTH=$(get_password_policy_key "min_length")
-  local UPPERCASE_FLAG=$(get_password_policy_key "must_contain_capital_letter")
-  local LOWERCASE_FLAG=$(get_password_policy_key "must_contain_lower_case_letter")
-  local DIGITS_FLAG=$(get_password_policy_key "must_contain_digit")
-  local SPECIAL_FLAG=$(get_password_policy_key "must_contain_special_character")
+  local MIN_LENGTH UPPERCASE_FLAG LOWERCASE_FLAG DIGITS_FLAG SPECIAL_FLAG
+
+  MIN_LENGTH=$(get_password_policy_key "min_length")
+  UPPERCASE_FLAG=$(get_password_policy_key "must_contain_capital_letter")
+  LOWERCASE_FLAG=$(get_password_policy_key "must_contain_lower_case_letter")
+  DIGITS_FLAG=$(get_password_policy_key "must_contain_digit")
+  SPECIAL_FLAG=$(get_password_policy_key "must_contain_special_character")
   echo "Retrieving config values end"
 
-  if  [[ -z "${MIN_LENGTH// /}" ]]; then
-     MIN_LENGTH="8"; # reset to standard
+  if  [[ -z "${MIN_LENGTH// }" ]]; then
+     MIN_LENGTH = 8; # reset to standard
   fi
 
   local UPPERCASE=""
@@ -38,7 +39,8 @@ function update_password_policy_setting(){
     SPECIAL="special_chars"
   fi
 
-  local SETTINGS_JSON=$(build_password_policy_settings_json "${MIN_LENGTH}" "${UPPERCASE}" "${LOWERCASE}" "${DIGITS}" "${SPECIAL}")
+  local SETTINGS_JSON
+  SETTINGS_JSON=$(build_password_policy_settings_json "${MIN_LENGTH}" "${UPPERCASE}" "${LOWERCASE}" "${DIGITS}" "${SPECIAL}")
   echo "Calling extended rest api start"
 
   # clean up json in case any of the array vals are not filled
@@ -49,12 +51,14 @@ function update_password_policy_setting(){
 
 function get_password_policy_key(){
   local configKey=${1}
-  local configValue=$(doguctl config -g -d " " password-policy/${configKey})
+  local configValue
+  configValue=$(doguctl config -g -d " " password-policy/"${configKey}")
   echo "${configValue}"
 }
 
 function build_password_policy_settings_json(){
-    local SETTINGS_JSON='{"password_min_length":"%s","password_required_char_classes":["%s","%s","%s","%s"]}'
+    local SETTINGS_JSON
+    SETTINGS_JSON='{"password_min_length":"%s","password_required_char_classes":["%s","%s","%s","%s"]}'
 
     local MIN_LENGTH="${1}"
     local UPPERCASE="${2}"
